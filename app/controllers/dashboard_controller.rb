5fcdba7fdb5ffdad128a8ee5493cb96152a505ee
harroyoy0610 <humberto-_-lzc@hotmail.com>
2018-05-29 16:26:58 -0500

    third co

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,10 +8,7 @@ class DashboardController < ActionController::Base
   	p "Paso1 "* 100 
   	p params[:person]
 
-		DashboardMailer.email_something_html("#{params[:person][:email]}",  "#{params[:person][:nombre]}", "#{params[:person][:mensaje]}").deliver_now
-		# sp = SparkPost::Client.new() # api key was set in ENV through ENV['SPARKPOST_API_KEY']
-		# response = sp.transmission.send_message('humberto-_-lzc@hotmail.com', 'huarci@gmail.com', 'huarci@gmail.com', '<h1>HTML message</h1>')
-		# puts response
+		DashboardMailer.email_something_html("#{params[:person][:email]}",  "#{params[:person][:nombre]}", "#{params[:person][:mensaje]}").deliver
 		p "Paso "* 100 
 		redirect_to root_path
 	end
