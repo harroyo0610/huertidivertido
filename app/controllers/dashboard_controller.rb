b8df5660f31671266daa1efd32a80dcfd9e554ea
harroyoy0610 <humberto-_-lzc@hotmail.com>
2018-05-29 12:42:17 -0500

    third commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ActionController::Base
 
   def create
 		DashboardMailer.email_something_html("#{params[:email]}", "#{params[:nombre]}", "<p>#{params[:mensaje]}</p>").deliver_now
+		redirect_to root
 	end
 
 end
