040d37d0d0e5a10cfe8d74a649b0cd54bbbb5eb6
harroyoy0610 <humberto-_-lzc@hotmail.com>
2018-05-29 13:53:52 -0500

    third commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ActionController::Base
 
   def create
   	p "Paso1 "* 100 
-		#DashboardMailer.email_something_html("#{params[:email]}", "#{params[:nombre]}", "<p>#{params[:mensaje]}</p>").deliver_now
+		DashboardMailer.email_something_html("#{params[:email]}", "#{params[:nombre]}", "<p>#{params[:mensaje]}</p>").deliver_now
 		p "Paso "* 100 
 		redirect_to root_path
 	end
