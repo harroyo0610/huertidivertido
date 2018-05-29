f46c10000def690de5fa0982b8c7b570226f403e
harroyoy0610 <humberto-_-lzc@hotmail.com>
2018-05-29 15:36:56 -0500

    third comm

--- app/mailers/dashboard_mailer.rb
+++ app/mailers/dashboard_mailer.rb
@@ -1,10 +1,7 @@
-class DashboardMailer < ActionMailer::Base
-	default from: "huarci@gmail.com"
+class DashboardMailer < ActionMailer
+	#default from: "huarci@gmail.com"
 
   def email_something_html(somefrom, somesubject, somebody)
-    mail(to: somefrom,
-         subject: somesubject,
-         body: somebody,
-         content_type: "text/html")
+    mail to: somefrom, subject: somesubject, body: somebody, content_type: "text/html"
   end
 end
