a6ab51dbf82ce0d67c828ec1cc87ae528d7dc3a8
harroyoy0610 <humberto-_-lzc@hotmail.com>
2018-05-29 13:46:41 -0500

    third commit

--- app/mailers/dashboard_mailer.rb
+++ app/mailers/dashboard_mailer.rb
@@ -1,9 +1,8 @@
 class DashboardMailer < ActionMailer::Base
-	#default to: "huarci@gmail.com"
+	default to: "huarci@gmail.com"
 
   def email_something_html(somefrom, somesubject, somebody)
     mail(from: somefrom,
-    			to: "huarci@gmail.com",
          subject: somesubject,
          body: somebody,
          content_type: "text/html")
